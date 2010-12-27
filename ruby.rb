be88c6228f1ec1c074e2871998e50090ba58bb70
meh <meh@paranoici.org>
2010-12-27 20:57:36 +0100

    Initial commit

---
+++ ruby.rb
@@ -0,0 +1,47 @@
+# using gem as backend
+
+require 'nokogiri'
+
+repository.do {
+  def each_package (&block)
+    dom = Nokogiri::XML.parse(filesystem.data.to_s)
+
+    `gem list --remote`.lines.each {|line|
+      CLI.info "Parsing `#{line.chomp}`" if System.env[:VERBOSE]
+
+      t, name, version = line.match(/^(.+?) \((.+?)([,\s].*)?\)$/).to_a
+
+      unless name && version
+        CLI.warn "`#{line.chomp}` was not parsed succesfully" if System.env[:VERBOSE]
+        next
+      end
+
+      block.call(Package.new(
+        :tags    => ['ruby', 'gem'] + ((dom.xpath(%{//gem[name = "#{name}"]/tags}).first.text.split(/\s+/) rescue nil) || []),
+        :name    => name,
+        :version => version
+      ))
+    }
+  end
+
+  def install (name)
+
+  end
+
+  def uninstall (name)
+
+  end
+}
+
+__END__
+$$$
+
+$$$ data $$$
+
+<data>
+  <gem name="dm-sqlite-adapter">
+    <tags>datamapper database</tags>
+
+    <dependency>database/sqlite</dependency>
+  </gem>
+</data>
